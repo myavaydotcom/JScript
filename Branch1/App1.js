37a0e6a24e5ee68e06e9d522b206ddafb02c75bd
myavaydotcom <45978193+myavaydotcom@users.noreply.github.com>
2019-12-10 11:23:38 +0530

    Branch1 has been added to master

---
+++ Branch1/App1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View , Text} from 'react-native';
+
+export default class NewApp extends React.Component{
+    render(){
+        return (
+            <Text>this is branch 1</Text>
+        )
+    }
+}
