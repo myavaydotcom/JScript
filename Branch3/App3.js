efcf91d2944624a6bd44fb2f9d60ed36074a3c76
myavaydotcom <45978193+myavaydotcom@users.noreply.github.com>
2019-12-10 12:12:25 +0530

    App3.js has been introduced

--- Branch3/App3.js
+++ Branch3/App3.js
@@ -1,23 +1,23 @@
-import React , {Component} from 'react';
-import {View , Button} from 'react-native';
+import React, { Component } from 'react';
+import { View, Button } from 'react-native';
 
 const App = (props) => {
-      return (
-          <View>
-              <Button title="click here" onPress={()=> null}/>
-              <Xyz name={"Anonymous"} />
-          </View>
-      )
+    return (
+        <View>
+            <Button title="click here" onPress={() => null} />
+            <Xyz name={"Anonymous"} />
+        </View>
+    )
 }
 
-class Xyz extends React.Component{
-    constructor(props){
+class Xyz extends React.Component {
+    constructor(props) {
         super(props);
     }
-    render(){
-        return(
+    render() {
+        return (
             <View>
-                 <Button title={this.props.name} onPress={() => null}/>
+                <Button title={this.props.name} onPress={() => null} />
             </View>
         )
     }
